053deaec978dc63ae1ca84c4e33aba1d3c346c82
Shi-Hao Hong <shihaohong94@gmail.com>
2017-06-22 15:07:05 -0700

    implement server hello world basic

---
+++ server/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, function(req, res) {
+  console.log('App running on port ' + port);
+})
