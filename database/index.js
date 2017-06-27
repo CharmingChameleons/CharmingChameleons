268454e29cb61744b199c3f6c9aa8df21d21bc64
Henri Pietila <henrismail@gmail.com>
2017-06-27 16:37:14 -0700

    heroku db config

--- database/index.js
+++ database/index.js
@@ -1,19 +1,32 @@
 var pg = require('pg');
 var Promise = require('bluebird');
 
-var config = {
-  user: 'sara', // name of the user account
-  database: 'shareio', // name of the database
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+var host = "localhost";
+var password = 'test';
+
+if (process.env.DATABASE_URL) {
+  host = process.env.DATABASE_URL;
+  password = 'password';
 }
 
+var config = {
+  user: 'henri', // name of the user account
+  host: host,
+  password: password,
+  database: 'henri', // name of the database
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000
+ // how long a client is allowed to remain idle before being closed
+}
+
+//create connection
 var pool = new pg.Pool(config)
 
  module.exports = {
- 
- //Select all listings
+
+ //returns all listings
  	getAllListings: () => {
+
 		return new Promise (
  			(resolve, reject) => {
  				pool.query('SELECT * from listings', function (err, result) {
@@ -22,96 +35,115 @@ var pool = new pg.Pool(config)
 				      reject(err)
 				    }
 				    else {
-				    	console.log('In getlistings', result)
+				    	console.log('got all the listings')
 				    	resolve(JSON.parse(JSON.stringify(result.rows)))
 				    }
-				})			
+				})
   		}
  		)
  	},
+ 	//Input: replace the following with its values[name, description, cost, tags]
+ 	//Output: returns the id of the listing object created => [{id: 1}]
+ 	createListing: (params) => {
+		console.log(params);
+		var queryString = 'INSERT INTO listings (name, description, cost, tags) VALUES ($1, $2, $3, $4) returning id'
+		var queryArgs = params
 
-	//create new booking
-	// createBookings: (params) => {
-	// 	console.log(params);
-	// 	var queryString = 'INSERT INTO bookings (listingId, borrowerId) VALUES (params[0], params[1])'
-	// 	var queryArgs = params
+		return new Promise (
+			(resolve, reject) => {
+				pool.query(queryString, queryArgs, (err, rows) => {
+					if (err) {
+						reject (err)
+						console.log(err);
+					} else {
+						console.log('added this item to DB');
+						resolve(JSON.parse(JSON.stringify(rows.rows)))
+					}
+				})
+			}
+		)
+	},
+	//Input: Replace the following with its values[listingId, borrowerId] --> internal use
+  //Output: returns the id of the booking object created => [{id: 1}]
+	createBookings: (params) => {
+		console.log(params);
+		var queryString = 'INSERT INTO bookings (listingId, borrowerId) VALUES ($1, $2) returning id'
+		var queryArgs = params
 
-	// 	return new Promise (
-	// 		(resolve, reject) => {
-	// 			pool.query(queryString, queryArgs, (err, rows) => {
-	// 				if (err) {
-	// 					//reject (err)
-	// 					//console.log(err);
-	// 				} else {
-	// 					console.log('got the rows');
-	// 					//resolve(rows)
-	// 				}
-	// 			})
-	// 		}
-	// 	)
+		return new Promise (
+			(resolve, reject) => {
+				pool.query(queryString, queryArgs, (err, rows) => {
+					if (err) {
+						reject (err)
+						console.log(err);
+					} else {
+						console.log('got data from createBookings');
+						resolve(JSON.parse(JSON.stringify(rows.rows)))
+					}
+				})
+			}
+		)
+	},
+  //Input: Replace the following with its values[userid]
+  //Output: Returns all the listings that belongs to one user-> array
+	getListingsForUser: (params) => {
+		var queryString = 'SELECT * FROM listings LEFT OUTER JOIN bookings \
+							ON (listings.id = bookings.listingId) \
+						   	WHERE listings.lenderId = $1'
+		var queryArgs = params
 
-	// }
+		return new Promise (
+			(resolve, reject) => {
+				pool.query(queryString, queryArgs, (err, rows) => {
+					if (err) {
+						reject (err)
+					} else {
+						console.log('got listing for user');
+						resolve(JSON.parse(JSON.stringify(rows.rows)))
+					}
+				})
+			}
+		)
+	},
+
+	//Input: Replace the following with its values[userid]
+  //Output: Returns the row containing that user id -> array
+	getUserName: (params) => {
+		var queryString = "SELECT * FROM users WHERE id = $1"
+		var queryArgs = params
+
+		return new Promise (
+			(resolve, reject) => {
+				pool.query(queryString, queryArgs, (err, rows) => {
+					if (err) {
+						reject (err)
+					} else {
+						console.log('got username');
+						resolve(JSON.parse(JSON.stringify(rows.rows)))
+					}
+				})
+			}
+		)
+	},
+
+	//Input: Replace the following with its values['username']
+  //Output: Returns the row containing that name -> array
+	getUserId: (params) => {
+		var queryString = "SELECT * FROM users WHERE username = $1"
+		var queryArgs = params
+
+		return new Promise (
+			(resolve, reject) => {
+				pool.query(queryString, queryArgs, (err, rows) => {
+					if (err) {
+						reject (err)
+					} else {
+						console.log('user id');
+						resolve(JSON.parse(JSON.stringify(rows.rows)))
+					}
+				})
+			}
+		)
+	}
 
 }
-	//Get all listings for user
-	// getListingsForUser: (params) => {
- 
-	// 	var queryString = "SELECT * FROM listings LEFT OUTER JOIN bookings \
-	// 						ON (listings.id = booking.listingId) \
-	// 					   	WHERE listings.lenderId = ?"
-
-	// 	var queryArgs = params
-
-	// 	return new Promise (
-	// 		(resolve, reject) => {
-	// 			db.query(queryString, queryArgs, (err, rows) => {
-	// 				if (err) {
-	// 					reject (err)
-	// 				} else {
-	// 					resolve(rows)
-	// 				}
-	// 			})
-	// 		}
-	// 	)
-	// },
-
-	// //Get username based on id
-	// getUserName: (params) => {
-	// 	var queryString = "SELECT * FROM users WHERE id = ?"
-
-	// 	var queryArgs = params
-
-	// 	return new Promise (
-	// 		(resolve, reject) => {
-	// 			db.query(queryString, queryArgs, (err, rows) => {
-	// 				if (err) {
-	// 					reject (err)
-	// 				} else {
-	// 					resolve(rows)
-	// 				}
-	// 			})
-	// 		}
-	// 	)
-	// },
-
-	// //Get user id based on username
-	// getUserId: (params) => {
-	// 	var queryString = "SELECT * FROM users WHERE username = ?"
-
-	// 	var queryArgs = params
-
-	// 	return new Promise (
-	// 		(resolve, reject) => {
-	// 			db.query(queryString, queryArgs, (err, rows) => {
-	// 				if (err) {
-	// 					reject (err)
-	// 				} else {
-	// 					resolve(rows)
-	// 				}
-	// 			})
-	// 		}
-	// 	)
-	// }
-
-//}
-
