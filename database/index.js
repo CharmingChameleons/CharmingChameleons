a4f951d88c439c994f93e11f37e28d887da05592
sara3 <sadaqiq@gmail.com>
2017-06-27 14:13:45 -0700

    db connected

--- database/index.js
+++ database/index.js
@@ -1,132 +1,5 @@
-var sqlite3 = require('sqllite3').verbose();
-var Promise = require('bluebird')
-
-//Connect to SQlLite3 database
-<<<<<<< HEAD
-var db = new sqlite3.Database('./shareio.db');
-
-module.exports = {
-
-	//Select all listings
-	getListings: () => {
-
-		return new Promise (
-			(resolve, reject) => {
-				db.all("SELECT * FROM listings", function (err, rows) {
-					if (err) {
-						reject(err)
-					} else {
-						resolve(rows)
-					}
-				})
-			}
-		)
-	},
-
-	//create new booking
-	createBookings: (params) => {
-
-		var queryString = "INSERT INTO bookings (listingId, borrowerId) VALUES (?, ?)"
-
-		var queryArgs = params
-
-		return new Promise (
-			(resolve, reject) => {
-				db.query(queryString, queryArgs, (err, rows) => {
-					if (err) {
-						reject (err)
-					} else {
-						resolve(rows)
-					}
-				})
-			}
-		)
-
-	},
-
-	//Get all listings for user
-	getListingsForUser: (params) => {
- 
-		var queryString = "SELECT * FROM listings LEFT OUTER JOIN bookings \
-							ON (listings.id = booking.listingId) \
-						   	WHERE listings.lenderId = ?"
-
-		var queryArgs = params
-
-		return new Promise (
-			(resolve, reject) => {
-				db.query(queryString, queryArgs, (err, rows) => {
-					if (err) {
-						reject (err)
-					} else {
-						resolve(rows)
-					}
-				})
-			}
-		)
-	},
-
-	//Get username based on id
-	getUserName: (params) => {
-		var queryString = "SELECT * FROM users WHERE id = ?"
-
-		var queryArgs = params
-
-		return new Promise (
-			(resolve, reject) => {
-				db.query(queryString, queryArgs, (err, rows) => {
-					if (err) {
-						reject (err)
-					} else {
-						resolve(rows)
-					}
-				})
-			}
-		)
-	},
-
-	//Get user id based on username
-	getUserId: (params) => {
-		var queryString = "SELECT * FROM users WHERE username = ?"
-
-		var queryArgs = params
-
-		return new Promise (
-			(resolve, reject) => {
-				db.query(queryString, queryArgs, (err, rows) => {
-					if (err) {
-						reject (err)
-					} else {
-						resolve(rows)
-					}
-				})
-			}
-		)
-	}
-
-}
-=======
-//pg.defaults.ssl = true;
-//|| process.env.DATABASE_URL
-
-
-// var q = null;
-// const connectionStr = 'postgres://127.0.0.1/shareio';
-
-// pg.connect(connectionStr, function(err, client) {
-//   if (err) {
-//   	console.error(err);
-//   } else {
-// 	  console.log('Connected to postgres! Getting schemas...');
-// 	  query = client.query('Select * from listings')
-// 	  query.on('row', (row) => {
-// 	  	console.log(row)
-// 	  	q = row;
-// 	  })
-//   }
-// });
-
-// console.log('quesry outside',q);
+var pg = require('pg');
+var Promise = require('bluebird');
 
 var config = {
   user: 'sara', // name of the user account
@@ -136,51 +9,11 @@ var config = {
 }
 
 var pool = new pg.Pool(config)
-//var myClient;
 
-
-// module.exports.query = (text, values) => {
-//   //console.log('query:', text, values)
-//   console.log('query got called');
-//   return pool.query(text, values)
-// }
-
-// pool.query('SELECT * from users', function (err, result) {
-//     if (err) {
-//       console.log(err)
-//     }
-//     else {
-//     	console.log(result)
-//     }
-   
-//   });
-
-// pool.connect(function (err, client, done) {
-//   if (err) console.log(err)
-//   myClient = client
-//   myClient.query('SELECT * from users', function (err, result) {
-//     if (err) {
-//       console.log(err)
-//     }
-//     console.log(result)
-//   })
-// })
-
-
-
-
-//pg.connect(connectionStr);
-
-
-
-
-
-// console.log(client.query);
  module.exports = {
-
-// 	//Select all listings
+ 
+ //Select all listings
  	getAllListings: () => {
-
 		return new Promise (
  			(resolve, reject) => {
  				pool.query('SELECT * from listings', function (err, result) {
@@ -193,32 +26,33 @@ var pool = new pg.Pool(config)
 				    	resolve(JSON.parse(JSON.stringify(result.rows)))
 				    }
 				})			
-  			}
+  		}
  		)
- 	}
- }
+ 	},
 
-	// //create new booking
+	//create new booking
 	// createBookings: (params) => {
-
-	// 	var queryString = "INSERT INTO bookings (listingId, borrowerId) VALUES (?, ?)"
-
+	// 	console.log(params);
+	// 	var queryString = 'INSERT INTO bookings (listingId, borrowerId) VALUES (params[0], params[1])'
 	// 	var queryArgs = params
 
 	// 	return new Promise (
 	// 		(resolve, reject) => {
-	// 			db.query(queryString, queryArgs, (err, rows) => {
+	// 			pool.query(queryString, queryArgs, (err, rows) => {
 	// 				if (err) {
-	// 					reject (err)
+	// 					//reject (err)
+	// 					//console.log(err);
 	// 				} else {
-	// 					resolve(rows)
+	// 					console.log('got the rows');
+	// 					//resolve(rows)
 	// 				}
 	// 			})
 	// 		}
 	// 	)
 
-	// },
+	// }
 
+}
 	//Get all listings for user
 	// getListingsForUser: (params) => {
  
@@ -281,8 +115,3 @@ var pool = new pg.Pool(config)
 
 //}
 
-
-
-
-
->>>>>>> pool works
