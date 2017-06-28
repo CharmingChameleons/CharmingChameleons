14c0b0d60c20d5507c2776f4be19f3abd75f3590
priyankacodes <pshah83@gmail.com>
2017-06-27 19:49:49 -0700

    added middleware folder

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,4 +1,70 @@
+var Promise = require('bluebird')
 
 module.exports = {
-	
+	authenticate: (username, password) => {
+
+		return new Promise (
+			(resolve, reject) => {
+				var params = []
+				params.push(username)
+				db.getUserId(params)
+				    .then((user) => {
+
+				    	//Check for password
+				    	if (!user[0]) {
+				    		resolve(false)
+				    	} else {
+
+				    		console.log('In authenticate', util.compareHash(password, user[0].hash, user[0].salt))
+
+				    		//Verify whether hash stored in db is same as hash created by password
+				    		if (util.compareHash(password, user[0].hash, user[0].salt)) {
+				    			resolve(true)
+				    		} else {
+				    			resolve(false)
+				    		}
+
+				    		resolve(true)
+				    	}
+				    	//return user
+				    })
+				    .error((err) => {
+				    	reject(err)
+				    })
+		})	
+	}
 } 
+
+// //Use Passport Locale Strategy for authentication
+// passport.use(new LocalStrategy (  
+// 	(username, password, done) => {
+// 		var params = []
+// 		params.push(username)
+// 	    db.getUserId(params)
+// 	    .then((user) => {
+
+// 	    	//Check for password
+// 	    	if (!user) {
+// 	    		return done(null, false)
+// 	    	}
+
+// 	    	//Verify whether hash stored in db is same as hash created by password
+// 	    	if (!(util.compareHash(user.hash, password, user.salt))) {
+// 	    		return done(null, false)
+// 	    	}
+
+// 	    	return done (null, user)
+// 	    })
+// 	    .error((err) => {
+// 	    	return done(null, false)
+// 	    })
+// 	}
+// ))
+
+// //Create session and store session details in Postgres Db
+// app.use(session({  
+// 	secret: 'awesome',	
+// 	store: ,
+// 	resave: false,
+// 	saveUninitialized: false
+// }))
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var db = require('../database')
 var app = express();
 var util = require('./lib/hashUtils');
+var middleware = require('./middleware');
 
 var cors = require('cors');
 const passport = require('passport');   
@@ -18,81 +19,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/public')));
 
-// //Create session and store session details in Postgres Db
-// app.use(session({  
-// 	secret: 'awesome',	
-// 	store: ,
-// 	resave: false,
-// 	saveUninitialized: false
-// }))
-
 //Initialize passport and express
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Use Passport Locale Strategy for authentication
-passport.use(new LocalStrategy (  
-	(username, password, done) => {
-		var params = []
-		params.push(username)
-	    db.getUserId(params)
-	    .then((user) => {
-
-	    	//Check for password
-	    	if (!user) {
-	    		return done(null, false)
-	    	}
-
-	    	//Verify whether hash stored in db is same as hash created by password
-	    	if (!(util.compareHash(user.hash, password, user.salt))) {
-	    		return done(null, false)
-	    	}
-
-	    	return done (null, user)
-	    })
-	    .error((err) => {
-	    	return done(null, false)
-	    })
-	}
-))
-
-var authenticate = function (username, password) {
-
-	return new Promise (
-		(resolve, reject) => {
-			var params = []
-			params.push(username)
-			db.getUserId(params)
-			    .then((user) => {
-
-			    	//Check for password
-			    	if (!user[0]) {
-			    		resolve(false)
-			    	} else {
-
-			    		console.log('In authenticate', util.compareHash(password, user[0].hash, user[0].salt))
-
-			    		//Verify whether hash stored in db is same as hash created by password
-			    		if (util.compareHash(password, user[0].hash, user[0].salt)) {
-			    			resolve(true)
-			    		} else {
-			    			resolve(false)
-			    		}
-
-			    		resolve(true)
-			    	}
-			    	//return user
-			    })
-			    .error((err) => {
-			    	reject(err)
-			    })
-	})	
-}
-
 app.get('/login', function(req, res, next) {
 	//On login check whether new login details
 
-	authenticate(req.body.username, req.body.password)
+	middleware.authenticate(req.body.username, req.body.password)
 	.then((result) => {
 		if (result === true) {
 	    	res.setHeader(200).send('Login successful')
@@ -108,7 +42,7 @@ app.get('/login', function(req, res, next) {
 
 app.post('/signup', function(req, res, next) {
 
-	authenticate(req.body.username, req.body.password)
+	middleware.authenticate(req.body.username, req.body.password)
 	.then((result) => {
 		if(result) {
 			//User found
@@ -140,13 +74,6 @@ app.post('/signup', function(req, res, next) {
 		res.status(500).send('User not authenticated')
 	})
 });
-
-
-// db.getUserId(['Priyanka'])
-// .then((data) => {
-// 	console.log('In server index.js')
-// 	console.log(data);
-// })
 
 app.listen(port, function(req, res) {
   console.log('App running on port ' + port);
