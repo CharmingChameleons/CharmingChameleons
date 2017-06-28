29ac34f32e9605378ab2b8020107e9ceb6c02330
Shi-Hao Hong <shihaohong94@gmail.com>
2017-06-27 17:49:33 -0700

    implement listings get request to server and rendering of database information

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -3,12 +3,31 @@ import ListingsEntry from './ListingsEntry.jsx'
 import Grid from 'react-bootstrap/lib/Grid'
 import Row from 'react-bootstrap/lib/Row'
 import exampleData from '../dummyData.js'
+var $ = require('jquery');
 
 class Listings extends React.Component {
   constructor(props) {
     super(props);
-    this.listings = exampleData;
-    console.log(exampleData);
+    this.state = {
+      listings: []
+    };
+  }
+
+  componentDidMount() {
+    console.log('running ajax request');
+    $.ajax({
+      type: 'GET',
+      url: '/listings',
+      success: (data) => {
+        this.setState({
+          listings: JSON.parse(data)
+        })
+      },
+      error: (err) => {
+        console.log('failed', err);
+      }
+
+    });
   }
 
   render() {
@@ -17,7 +36,7 @@ class Listings extends React.Component {
         <h3>Listings:</h3>
         <Grid>
             <Row>
-            { this.listings.map((listing) => 
+            { this.state.listings.map((listing) => 
               <ListingsEntry key={listing.name} listing={listing}/>
             )}
             </Row>
